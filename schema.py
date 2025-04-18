de4862ec30e98812fff697076739f3bdaa301059
Nattawut-M <t.nattawut2020@hotmail.com>
2025-04-18 23:56:48 +0700

    feat: implement resolver for books query

--- schema.py
+++ schema.py
@@ -11,3 +11,20 @@ class Book:
 @strawberry.type
 class Query:
     books: typing.List[Book]
+
+
+def get_books():
+    return [
+        Book(
+            title="The Great Gatsby",
+            author="F. Scott Fitzgerald",
+        ),
+    ]
+
+
+@strawberry.type
+class Query:
+    books: typing.List[Book] = strawberry.field(resolver=get_books)
+
+
+schema = strawberry.Schema(query=Query)
